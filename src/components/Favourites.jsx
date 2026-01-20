9544cf060a49f0b0875c9490b15a3155d4d04427
Aleksa Subotin <akacar9@gmail.com>
2026-01-20 14:28:23 +0100

    favourites done

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,5 +1,8 @@
 import "../components/Favourites.css";
 import Marquee from "./Marquee";
+import cheesy from "../../src/assets/burger-cheesy.jpg";
+import maximus from "../../src/assets/burger-maximus.png";
+import shorty from "../../src/assets/burger-shorty.png";
 
 export default function Favourites() {
   return (
@@ -7,12 +10,11 @@ export default function Favourites() {
       <Marquee></Marquee>
       <div className="favourites-container-main">
         <div className="favourites-card">
-          <div className="favourites-img">PICTURE</div>
+          <img src={cheesy} alt="Cheesy burger" className="favourites-img" />
           <div className="favourites-header">CHEESY CRAVE</div>
           <div className="favourites-desc">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            fugit facere architecto neque molestias libero exercitationem, sint
-            asperiores
+            Melted cheddar dripping over a juicy patty, toasted bun, and just
+            enough crunch to keep things serious and satisfying.
           </div>
           <div className="favourites-cta">
             <a
@@ -25,12 +27,11 @@ export default function Favourites() {
           </div>
         </div>
         <div className="favourites-card">
-          <div className="favourites-img">PICTURE</div>
+          <img src={maximus} alt="Maximus burger" className="favourites-img" />
           <div className="favourites-header">MAXIMUS</div>
           <div className="favourites-desc">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            fugit facere architecto neque molestias libero exercitationem, sint
-            asperiores
+            A towering stack loaded with crispy bacon, crunchy nachos, melted
+            cheese, and bold sauces built for real hunger.
           </div>
           <div className="favourites-cta">
             <a
@@ -43,12 +44,11 @@ export default function Favourites() {
           </div>
         </div>
         <div className="favourites-card">
-          <div className="favourites-img">PICTURE</div>
+          <img src={shorty} alt="shorty burger" className="favourites-img" />
           <div className="favourites-header">SHORTY</div>
           <div className="favourites-desc">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            fugit facere architecto neque molestias libero exercitationem, sint
-            asperiores
+            Compact, juicy, and full of flavor — a small burger made for quick
+            bites without cutting corners on taste.
           </div>
           <div className="favourites-cta">
             <a
