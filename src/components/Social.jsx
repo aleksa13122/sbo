eef100c667f3257fde17683d5fed0fc9495acda0
Aleksa Subotin <akacar9@gmail.com>
2026-01-26 12:09:20 +0100

    Socials section sceleton done

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,15 +1,18 @@
 import "./Social.css";
-import character from "../assets/burger-chef.png";
+import character from "../assets/burger-cheff.png";
 
 export default function Brand() {
   return (
     <div className="social-container-main">
-      <h3>SOCIAL HEADER</h3>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, magni
-        ipsam! Quos omnis eaque inventore
-      </p>
-      <img className="social-img" src={character} />
+      <h3 className="social-header">PEOPLE SAY...</h3>
+      <div className="social-divs">
+        <p className="social-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />
+          Facilis, magni ipsam!
+          <p className="social-name">-Person who knows burgers good</p>
+        </p>
+        <img className="social-img" src={character} />
+      </div>
     </div>
   );
 }
