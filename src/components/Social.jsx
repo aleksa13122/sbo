1ca33491538c5cd64ad67585816609d9672770de
Aleksa Subotin <akacar9@gmail.com>
2026-01-26 17:15:15 +0100

    Socials section sceleton done

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,16 +1,31 @@
 import "./Social.css";
 import character from "../assets/burger-cheff.png";
+import { useState, useEffect } from "react";
 
 export default function Brand() {
+  const [index, setIndex] = useState(0);
+  const quotes = [
+    { text: "Very good burgers!", author: "-Person who knows burgers" },
+    { text: "Very good burgers1", author: "-Person who knows burgers1" },
+    { text: "Very good burgers2", author: "-Person who knows burgers2" },
+    { text: "Very good burgers3", author: "-Person who knows burgers3" },
+  ];
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % quotes.length);
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="social-container-main">
       <h3 className="social-header">PEOPLE SAY...</h3>
       <div className="social-divs">
-        <p className="social-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />
-          Facilis, magni ipsam!
-          <p className="social-name">-Person who knows burgers good</p>
-        </p>
+        <div className="social-text">
+          {quotes[index].text}
+          <p className="social-name">{quotes[index].author}</p>
+        </div>
         <img className="social-img" src={character} />
       </div>
     </div>
