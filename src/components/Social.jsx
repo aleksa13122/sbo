5e28aad672f37b07fb5968d8c5e9050b960fd7e4
Aleksa Subotin <akacar9@gmail.com>
2026-01-28 15:43:44 +0100

    Socials and brand sections fully done with animations

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,25 +3,14 @@ import character from "../assets/burger-cheff.png";
 import { useState, useEffect } from "react";
 
 export default function Brand() {
-  const [index, setIndex] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(false);
   const quotes = [
     {
       text: "Juicy, messy, absolutely worth it.”",
       author: "-Person who lives for burgers",
     },
-    {
-      text: "Serious flavour, no shortcuts taken.",
-      author: "-Burger chef",
-    },
-    {
-      text: "Best burger stop on my trip.",
-      author: "-Travel food influencer",
-    },
-    {
-      text: "You can taste the quality instantly.",
-      author: "-Local foodie",
-    },
+    { text: "Serious flavour, no shortcuts taken.", author: "-Burger chef" },
+    { text: "Best burger stop on my trip.", author: "-Travel food influencer" },
+    { text: "You can taste the quality instantly.", author: "-Local foodie" },
     {
       text: "Simple ingredients, insanely good result.",
       author: "-Person who knows burgers",
@@ -31,35 +20,42 @@ export default function Brand() {
       author: "-Regular who loves burgers",
     },
   ];
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setIndex((prev) => (prev + 1) % quotes.length);
-    }, 5000);
 
-    return () => clearInterval(timer);
+  const INTERVAL = 5000;
+  const DURATION = 350;
+
+  const [index, setIndex] = useState(0);
+  const [state, setState] = useState("show");
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      // fade + blur OUT
+      setState("hide");
+
+      // swap text while hidden
+      setTimeout(() => {
+        setIndex((prev) => (prev + 1) % quotes.length);
+
+        // fade + blur IN
+        setState("show");
+        setTimeout(() => DURATION);
+      }, DURATION);
+    }, INTERVAL);
+
+    return () => clearInterval(id);
   }, []);
-
-  useEffect(() => {
-    setIsAnimating(true);
-    const timeout = setTimeout(() => {
-      setIsAnimating(false);
-    }, 300);
-
-    return () => clearTimeout(timeout);
-  }, [index]); // Sta je u zagradi[] na njegovu promenu se radi efekat
 
   return (
     <div className="social-container-main">
       <h3 className="social-header">PEOPLE SAY...</h3>
+
       <div className="social-divs">
-        <div className={`social-text ${isAnimating ? "is-animating" : ""}`}>
+        <div className={`social-text ${state}`}>
           {quotes[index].text}
           <p className="social-name">{quotes[index].author}</p>
         </div>
-        <img
-          className={`social-img ${isAnimating ? "wiggle" : ""}`}
-          src={character}
-        />
+
+        <img className="social-img" src={character} alt="Burger chef" />
       </div>
     </div>
   );
